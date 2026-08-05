cbaeca58927087ee5c2c2a34a0d6b0aa7feee233
Abdullah-Al-Nahin <abdullahalnahin6@gmail.com>
2026-08-06 01:32:02 +0600

    Updated vite config to open browser

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true, // 👈 Forces Vite to launch Chrome / your default browser
+  },
 })
